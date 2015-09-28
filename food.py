ec307960e8c02dd8c757e1c84c0564e054d4130d
Mark Dang <markdang@gwu.edu>
2015-09-28 18:25:19 -0400

    Fixed Calorie search and ingredient search.

--- food.py
+++ food.py
@@ -3,7 +3,10 @@ from json import load
 import json
 import re
 
-ingredients = raw_input("What ingredients do you want?")
+#User is prompted to enter ingredients for recipes. ie: Chicken, rice, beans
+ingredients = raw_input("What ingredients do you want?\n")
+ingredients = ingredients.replace(",","")
+ingredients = ingredients.replace(" ", "+")
 url = "http://food2fork.com/api/search?key="
 key = "7ca33bbd3af2eaa85e40644cb479036f"
 url += key
@@ -19,42 +22,59 @@ json_obj = load(response)
 for i in json_obj['recipes']:
 	print i['title'] + ' - ' + i['recipe_id']
 
-rID = raw_input("Enter the ID number for the recipe you want.")
+#User selects the choosen recipe based on ID number of recipe. ie: 15300
+#A list of the ingredients is printed out.
+rID = raw_input("\nEnter the ID number for the recipe you want.\n")
 
 get_url = "http://food2fork.com/api/get?key="
 get_url += key
 get_url += "&rId="
 get_url += rID
 
+print get_url
+
 get_response = urlopen(get_url)
 get_final = load(get_response)
 
-calorie_api = "a70UXYhUSUx0bO3XF7SBAThe4gBAnbWLBfj3QWRR"
-calorie_url = " http://api.nal.usda.gov/ndb/search/?format=json&q="
+
 
 for ingredient in get_final['recipe']['ingredients']:
-	comma_replace = ingredient.replace(",","")
-	space_replace = comma_replace.replace(" ", "+")
-	calorie_url += space_replace
+	print ingredient
+
+total_calories = 0
+
+ingredient_calories = []
+
+while True:
+	calorie_api = "a70UXYhUSUx0bO3XF7SBAThe4gBAnbWLBfj3QWRR"
+	calorie_url = " http://api.nal.usda.gov/ndb/search/?format=json&q="
+
+	nutrient_url =  " http://api.nal.usda.gov/ndb/nutrients/?format=json&api_key="
+	nutrient_url += calorie_api
+	nutrient_url += "&nutrients=208&ndbno="
+
+	#From list of ingredients, user types the ingredient name to get its calories.
+	#Example: "2 cups of milk". Input: "milk"
+	calorie_search = raw_input("\nWhat ingredient do you want the calories for? Or type exit to see your total calories.\n")
+	calorie_search = calorie_search.replace(",", "")
+	calorie_search = calorie_search.replace(" ", "+")
+	if calorie_search == "exit":
+		print "\n"
+		for ingredient,calories in ingredient_calories:
+			print ingredient + " Calories: " + calories
+		print "Your total calories are %d" % total_calories
+		break 
+	calorie_url += calorie_search
 	calorie_url += "&sort=n&max=25&offset=0&api_key="
 	calorie_url += calorie_api
-	print calorie_url
 	calorie_response = urlopen(calorie_url)
 	calorie_json_obj = load(calorie_response)
-
 	for result in calorie_json_obj['list']['item']:
 		print result['name'] + ' -- ' + result['ndbno']
-
-#first_ingredient = "6 skinless, boneless chicken breast halves"
-#first_ingredient_second = first_ingredient.replace(",", "")
-#first_ingredient_third = first_ingredient_second.replace(" ", "+")
-#print first_ingredient_third
-
-#calorie_url += first_ingredient_third
-#calorie_url += "&sort=n&max=25&offset=0&api_key="
-#calorie_url += calorie_api
-#print calorie_url
-#calorie_response = urlopen(calorie_url)
-#calorie_json_obj = load(calorie_response)
-#for result in calorie_json_obj['list']['item']:
-	#print result['name'] + ' -- ' + result['ndbno']
+	calorie_search = raw_input("\nEnter the ID for your ingredient\n")
+	nutrient_url += calorie_search
+	nutrient_response = urlopen(nutrient_url)
+	nutrient_json_obj = load(nutrient_response)
+	ingredient_calories.append((nutrient_json_obj['report']['foods'][0]['name'],nutrient_json_obj['report']['foods'][0]['nutrients'][0]["value"]))
+	print nutrient_json_obj['report']['foods'][0]['name'] + " Calories: " + nutrient_json_obj['report']['foods'][0]['nutrients'][0]["value"]
+	total_calories += int(nutrient_json_obj['report']['foods'][0]['nutrients'][0]["value"])
